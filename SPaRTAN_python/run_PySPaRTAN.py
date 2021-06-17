26bdae30228c997c945fe1b1fba80f128e0308fd
xim2016 <xj_ma@yahoo.com>
2021-06-17 17:07:22 -0400

    Update run_PySPaRTAN.py
    add documentations for the code

--- SPaRTAN_python/run_PySPaRTAN.py
+++ SPaRTAN_python/run_PySPaRTAN.py
@@ -1,32 +1,67 @@
-import sys
+"""
+This script intends to use PySPaRTAN module to generate predicted matrices used in the paper.
+
+PySPaRTAN has 3 Hyperparameters:Spectrum P, lamda, and rsL2
+We can run PySPaRTAN by specifying some values to those parameters or using default ones in the script.
+We can also use cross-validation to generate the optional values for those
+Hyperparameters at first, and then run PySPaRTAN to generate the projections.
+
+
+When running this script from command line, the following parameters can be added to the command:
+    --input_dir : directory of input files, default="../data/inputs"
+    --output_dir : directory of output files, default="../data/outputs"
+    --dataset_D : the name of gene-TF matrix file.
+                  the file requires .csv format.
+                  only contains name here, do not include ".csv" extension
+    --dataset_P : the name of protein matrix
+                  the same other requirements as --dataset_D
+    --dataset_Y : the name of gene expression matrix
+                  the same other requirements as --dataset_D
+    --spectrumP : Dimension reduction coefficient on protein space, default=0.7  
+    --lamda :  LASSO regression coefficient,default=0.001
+    --rsL2 : ridge regression coefficient, default=0.001
+    --normalization : type of normalizion performed on matrices,
+                      default is l2 normalization
+    --fold : how many folds to be used when doing cross-validation.
+             default=0, means using specified hyper-parameters, do not do cross-validation
+             
+             
+This script requires numpy, pandas, sklearn to be installed in the python running environment
+
+"""
+import argparse
 import os
-import random
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import normalize
-from scipy.io import loadmat
-from scipy import spatial
-from scipy import stats
 from PySPaRTAN import PySPaRTAN
 
-import argparse
-
 parser = argparse.ArgumentParser()
-parser.add_argument("--dataset_D", help="name of the dataset D which will be passed in", type=str, default="Dpbmc")
-parser.add_argument("--dataset_P", help="name of the dataset P which will be passed in", type=str, default="Ppbmc5kn_CD8")
-parser.add_argument("--dataset_Y", help="name of the dataset Y which will be passed in", type=str, default="Ypbmc5kn_CD8")
-parser.add_argument("--spectrumP", help="Dimension reduction coefficient on protein space", type=float, default=0.7)
-parser.add_argument("--lamda", help="LASSO regression coefficient", type=float, default=0.001)
-parser.add_argument("--rsL2", help="ridge regression coefficient", type=float, default=0.001)
-parser.add_argument("--normalization", help="type of normalizion, no normalization if set to empty", type=str, default="l2")
-parser.add_argument("--input_dir", help="directory of input files", type=str, default="../data/inputs")
-parser.add_argument("--output_dir", help="directory of output files", type=str, default="../data/outputs")
-parser.add_argument('--fold', help="how many folds for the cross_validation.No cross_validation and using default/specified parameters if set to 0", type=int, default=0)
+
+parser.add_argument("--input_dir", help="directory of input files",
+                    type=str, default="../data/inputs")
+parser.add_argument("--output_dir", help="directory of output files",
+                    type=str, default="../data/outputs")
+parser.add_argument("--dataset_D", help="name of gene-TF matrix",
+                    type=str, default="Dpbmc")
+parser.add_argument("--dataset_P", help="name of the dataset P which will be passed in",
+                    type=str, default="Ppbmc5kn_CD8")
+parser.add_argument("--dataset_Y", help="name of the dataset Y which will be passed in",
+                    type=str, default="Ypbmc5kn_CD8")
+parser.add_argument("--spectrumP", help="Dimension reduction coefficient on protein space",
+                    type=float, default=0.7)
+parser.add_argument("--lamda", help="LASSO regression coefficient",
+                    type=float, default=0.001)
+parser.add_argument("--rsL2", help="ridge regression coefficient",
+                    type=float, default=0.001)
+parser.add_argument("--normalization", help="type of normalizion performed on matrices,\
+                    no normalization if set to empty", type=str, default="l2")
+parser.add_argument('--fold', help="how many folds for the cross_validation.\
+                    No cross_validation and using default/specified parameters if set to 0",
+                    type=int, default=0)
 
 args = parser.parse_args()
-
-
 
 if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
@@ -54,11 +89,9 @@ if args.normalization != "":
 # create the object of SPaRTAN
 reg = PySPaRTAN()
 
-
-
-# cross-validate to determine hyperparameters
+# cross-validate to determine optimal parameters
 fold = args.fold
-if fold != 0: #using cross validation to determine the optimal parameters
+if fold != 0:  # using cross validation to determine the optimal parameters
     
     lamdas = [0.001, 0.01, 0.1, 0.2, 0.3]
     rsL2s = [0, 0.001, 0.01]
@@ -75,11 +108,8 @@ if fold != 0: #using cross validation to determine the optimal parameters
         for b in range(0, lenspBs):
             for l in range(0, lenlamdas):
                 for r in range(0, lenrsL2s):
-                    print(
-                        "cross validating spectrumA={}, spectrumB={}, lambda={}, rsL2={}".format(
-                            spectrumAs[a], spectrumBs[b], lamdas[l], rsL2s[r]
-                        )
-                    )
+                    print("cross validating spectrumA={}, spectrumB={}, lambda={}, rsL2={}"
+                          .format(spectrumAs[a], spectrumBs[b], lamdas[l], rsL2s[r]))
                     sum_corr_spearman = 0
     
                     kf = KFold(n_splits=fold)
@@ -116,8 +146,7 @@ if fold != 0: #using cross validation to determine the optimal parameters
                         sum_corr_spearman = sum_corr_spearman + corr_spearman
     
                     corr_all_spearman[a, b, l, r] = sum_corr_spearman / fold
-                    
-   
+
     # retrive the best parameters
     max_a, max_b, max_l, max_r = np.unravel_index(
         corr_all_spearman.argmax(), corr_all_spearman.shape
@@ -128,15 +157,15 @@ if fold != 0: #using cross validation to determine the optimal parameters
     spectrumA_best = spectrumAs[max_a]
     spectrumB_best = spectrumBs[max_b]
        
-    print("lamda_best={}, rsL2_best={}, spectrumA_best={},spectrumB_best={}".format(lamda_best,rsL2_best,spectrumA_best,spectrumB_best))
-             
-                    
-else: #fold ==0: using default/specified paramters
- 
-    lamda_best=args.lamda
-    rsL2_best=args.rsL2
-    spectrumA_best=1
-    spectrumB_best=args.spectrumP
+    print("lamda_best={}, rsL2_best={}, spectrumA_best={}, spectrumB_best={}"
+          .format(lamda_best, rsL2_best, spectrumA_best, spectrumB_best))
+
+else:  # fold ==0: using default/specified paramters
+
+    lamda_best = args.lamda
+    rsL2_best = args.rsL2
+    spectrumA_best = 1
+    spectrumB_best = args.spectrumP
 
 print("Processing ...")
 # re-train the model
@@ -160,13 +189,5 @@ df_W.to_csv(outfile_W)
 df_projP.to_csv(outfile_projP)
 df_projD.to_csv(outfile_projD)
 
-# with open(outfile_W, "w") as outfile:
-    # np.savetxt(outfile, W, fmt="%-7.4f")
-
-# with open(outfile_projP, "w") as outfile:
-    # np.savetxt(outfile, projP, fmt="%-7.4f")
-
-# with open(outfile_projD, "w") as outfile:
-    # np.savetxt(outfile, projD, fmt="%-7.4f")
 
 print("Process finished successfully!")
