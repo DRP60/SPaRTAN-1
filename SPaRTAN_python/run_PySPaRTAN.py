6742d174e86bec6f02f9f09ef7d804c30b910608
xim2016 <xj_ma@yahoo.com>
2021-06-17 17:42:37 -0400

    Update run_PySPaRTAN.py

--- SPaRTAN_python/run_PySPaRTAN.py
+++ SPaRTAN_python/run_PySPaRTAN.py
@@ -46,9 +46,9 @@ parser.add_argument("--output_dir", help="directory of output files",
 parser.add_argument("--dataset_D", help="name of gene-TF matrix",
                     type=str, default="Dpbmc")
 parser.add_argument("--dataset_P", help="name of the dataset P which will be passed in",
-                    type=str, default="Ppbmc5kn_CD8")
+                    type=str, default="Ppbmc5kn_CD16")
 parser.add_argument("--dataset_Y", help="name of the dataset Y which will be passed in",
-                    type=str, default="Ypbmc5kn_CD8")
+                    type=str, default="Ypbmc5kn_CD16")
 parser.add_argument("--spectrumP", help="Dimension reduction coefficient on protein space",
                     type=float, default=0.7)
 parser.add_argument("--lamda", help="LASSO regression coefficient",
@@ -59,7 +59,7 @@ parser.add_argument("--normalization", help="type of normalizion performed on ma
                     no normalization if set to empty", type=str, default="l2")
 parser.add_argument('--fold', help="how many folds for the cross_validation.\
                     No cross_validation and using default/specified parameters if set to 0",
-                    type=int, default=0)
+                    type=int, default=5)
 
 args = parser.parse_args()
 
@@ -94,7 +94,7 @@ fold = args.fold
 if fold != 0:  # using cross validation to determine the optimal parameters
     
     lamdas = [0.001, 0.01, 0.1, 0.2, 0.3]
-    rsL2s = [0, 0.001, 0.01]
+    rsL2s = [0.001, 0.01, 0.1]
     spectrumAs = [1]
     spectrumBs = [0.5, 0.6, 0.7]
 
@@ -103,14 +103,14 @@ if fold != 0:  # using cross validation to determine the optimal parameters
     lenspAs = len(spectrumAs)
     lenspBs = len(spectrumBs)
     
-    corr_all_spearman = np.zeros((lenspAs, lenspBs, lenlamdas, lenrsL2s))
+    corr_all_pearson = np.zeros((lenspAs, lenspBs, lenlamdas, lenrsL2s))
     for a in range(0, lenspAs):
         for b in range(0, lenspBs):
             for l in range(0, lenlamdas):
                 for r in range(0, lenrsL2s):
-                    print("cross validating spectrumA={}, spectrumB={}, lambda={}, rsL2={}"
-                          .format(spectrumAs[a], spectrumBs[b], lamdas[l], rsL2s[r]))
-                    sum_corr_spearman = 0
+                    print("cross validating spectrumP={}, lambda={}, rsL2={}"
+                          .format(spectrumBs[b], lamdas[l], rsL2s[r]))
+                    sum_corr_pearson = 0
     
                     kf = KFold(n_splits=fold)
                     for train_index, test_index in kf.split(P_mat):
@@ -141,15 +141,15 @@ if fold != 0:  # using cross validation to determine the optimal parameters
                         Y_pred = reg.predict(P_test)
     
                         # get the correlation bewteen Y_pred and Y_test
-                        corr_spearman = reg.get_corr(Y_pred, Y_test)
+                        corr_pearson = reg.get_corr(Y_pred, Y_test)
     
-                        sum_corr_spearman = sum_corr_spearman + corr_spearman
+                        sum_corr_pearson = sum_corr_pearson + corr_pearson
     
-                    corr_all_spearman[a, b, l, r] = sum_corr_spearman / fold
+                    corr_all_pearson[a, b, l, r] = sum_corr_pearson / fold
 
     # retrive the best parameters
     max_a, max_b, max_l, max_r = np.unravel_index(
-        corr_all_spearman.argmax(), corr_all_spearman.shape
+        corr_all_pearson.argmax(), corr_all_pearson.shape
     )
     
     lamda_best = lamdas[max_l]
